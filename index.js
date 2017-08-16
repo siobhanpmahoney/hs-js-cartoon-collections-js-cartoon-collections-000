5c1c96adef29f892f25402156bb97035abe7c10d
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-08-16 12:24:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+  for (let i = 0; i<dwarves.length; i++) {
+    dwarves[i] = `${i+1}. ${dwarves[i]}`
+    }
+  var dwarvesOne = "";
+  for (let i = 0; i<dwarves.length; i++) {
+    dwarvesOne = dwarvesOne + dwarves[i] + " ";
+  }
+  return dwarvesOne
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (let i=0; i<planeteerCalls.length; i++) {
+    planeteerCalls[i] = `${planeteerCalls[i].toUpperCase}!`
+  }
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i<words.length; i++) {
+    if (words[i].length>4)
+    return true;
+  }
+  return false;
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+  var cheese = ["cheddar", "gouda", "camembert"];
+  for (let i = 0; i<foods.length; i++) {
+    if (cheese.includes(foods[i])) {
+      return foods[i];
+    }
+  }
+    return "no cheese!"
 }
